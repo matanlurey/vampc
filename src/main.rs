a78ae7300940b92a577217939aae285e388e8f9a
Matan Lurey <matan@lurey.org>
2020-08-08 16:27:07 -0700

    Cleaner scanner start.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,7 @@
-use std::env;
-use std::fs;
-
 #[allow(dead_code)]
-mod parser;
 mod scanner;
 
-/// Entrypoint into the compiler/interpreter/virtual machine, what have it.
-///
-/// # Usage
-///
-/// ```sh
-/// vampc <script>
-/// ```
+// TODO: Add a command-line tool that parses and executes code.
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  match args.len() - 1 {
-    0 => {
-      println!("REPL not yet implemented");
-      std::process::exit(1);
-    }
-    1 => {
-      let file = &args[1];
-      load_and_run(file);
-    }
-    _ => {
-      println!("Usage: vampc <script>");
-      std::process::exit(1);
-    }
-  }
-}
-
-/// Parses and runs a program from source.
-///
-/// Currently only a single file program is supported.
-fn run_source(source: String) {
-  let mut scanner = scanner::Scanner::new(source);
-  scanner.scan();
-  let mut parser = parser::Parser::new(scanner.output);
-  parser.parse();
-  for ast in parser.output {
-    println!("{:?}", ast);
-  }
-}
-
-/// Loads a file and passes it to `run_source`.
-fn load_and_run(input: &str) {
-  let result = fs::read_to_string(input);
-  let contents = result.expect("Could not read file");
-  run_source(contents);
+  panic!("Command-line access not yet supported! Use `cargo test` instead.");
 }
