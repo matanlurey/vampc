dc464aa50e84e2c30652d5697a63b13551ddde78
Matan Lurey <matan@lurey.org>
2020-08-08 12:57:34 -0700

    Annoyed checkpoint.

--- src/parser.rs
+++ src/parser.rs
@@ -60,16 +60,19 @@ impl Parser {
     let mut tokens = self.input.iter().peekable();
     while let Some(next) = tokens.next() {
       let declaration: Option<Declaration> = match next {
-        Token::Comment(comment) => {
-          let text = Parser::parse_comment_contents(&mut tokens);
-          Some(Declaration::Comment { text })
-        }
+        Token::Comment(comment) => Some(Declaration::Comment {
+          text: Parser::parse_comment_contents(comment, &mut tokens),
+        }),
         _ => panic!("Unexpected"),
       };
+      if let Some(declaration) = declaration {
+        self.output.push(declaration);
+      }
     }
   }
 
   fn parse_comment_contents<T: Iterator<Item = Token>>(
+    initial: &str,
     tokens: &mut iter::Peekable<T>,
   ) -> String {
     String::from("")
@@ -85,5 +88,21 @@ impl Parser {
 
   fn parse_expression() -> Option<Expression> {
     None
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn assert_tree(input: Vec<Token>, output: &[Declaration]) {
+    let mut parser = Parser::new(input);
+    parser.parse();
+    assert_eq!(output.len(), parser.output.len());
+  }
+
+  #[test]
+  fn test_top_level_comments() {
+    assert_tree(vec![Token::Comment(String::from("Hello World"))], &[]);
   }
 }
